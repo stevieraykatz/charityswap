47f31ad0e36f6cf48026ce99dc70e3a9e2227ac1
katzman <steve.r.katzman@gmail.com>
2022-09-07 19:43:09 +0000

    Setup typechain project

---
+++ hardhat.config.ts
@@ -0,0 +1,26 @@
+import * as dotenv from "dotenv";
+
+import { HardhatUserConfig, task } from "hardhat/config";
+import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-waffle";
+import "@typechain/hardhat";
+import "solidity-coverage";
+
+dotenv.config();
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.10",
+  networks: {},
+  etherscan: {},
+  
+};
+
+export default config;
