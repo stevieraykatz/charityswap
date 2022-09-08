8401e65d370f9d04ea36906daeeeed6ac4b3208d
katzman <steve.r.katzman@gmail.com>
2022-09-08 07:32:49 +0000

    Cleaned up diamond from previous project,

--- contracts/libraries/AppStorage.sol
+++ contracts/libraries/AppStorage.sol
@@ -1,25 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-struct Account {
-    // spender => amount    
-    mapping(address => bool) ticketsApproved;    
-    uint40 lastFrensUpdate;    
-    uint256 poolTokens;
-    uint256 frens;    
-}
-
-struct Ticket {
-    // user address => balance
-    mapping(address => uint256) accountBalances;
-    uint256 totalSupply;
+struct Charity {
+    address charityAddress;
+    mapping(address => bool) tokenAllowed;    
 }
 
 struct AppStorage {
-    mapping(address => Account) accounts;
-    mapping(uint256 => Ticket) tickets;    
-    address poolContract;
-    string ticketsBaseUri;    
-    uint256 poolTokensRate;
-    address marketPlaceDiamond ;
+    mapping(uint256 => Charity) CharitiesById;
+    address marketPlaceDiamond;
 }
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -2,7 +2,9 @@
 import { ethers } from "hardhat";
 import * as logger from "../utils/logger"
 import * as fs from "fs";
-import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers"
+
+const diamond = require('diamond-util')
 
 async function deploy() {
 
@@ -15,21 +17,21 @@ async function deploy() {
   logger.out("Deploying to: " + network.name, logger.Level.Info)
   logger.out("With chain id: " + network.chainId, logger.Level.Info)
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(3654568416);
+  const deployedDiamond = await diamond.deploy({
+    diamondName: 'StakingDiamond',
+    facets: [
+      'DiamondCutFacet',
+      'DiamondLoupeFacet',
+      'OwnershipFacet'
+    ],
+    args: [deployer]
+  })
 
-  await lock.deployed();
-
-  logger.pad(30, "Deployed to:", lock.address);
-
-  logger.divider()
-  logger.out("Writing to address.json", logger.Level.Info)
-
-  let rawdata = fs.readFileSync('address.json', "utf8")
-  let address: any = JSON.parse(rawdata)
-  address[network.chainId] = lock.address
-  const json = JSON.stringify(address, null, 2)
-  fs.writeFileSync('address.json', json, "utf8")
+  // let rawdata = fs.readFileSync('address.json', "utf8")
+  // let address: any = JSON.parse(rawdata)
+  // address[network.chainId] = lock.address
+  // const json = JSON.stringify(address, null, 2)
+  // fs.writeFileSync('address.json', json, "utf8")
 }
 
 deploy().catch((error) => {
